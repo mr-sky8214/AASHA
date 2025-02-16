254c154ba8a342661aaad8038de742f504313226
Akash Kamble <kambleakash8214@gmail.com>
2025-02-16 15:36:36 +0530

    Initial Setp up

---
+++ src/main/java/com/aasha/AashaApplication.java
@@ -0,0 +1,13 @@
+package com.aasha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AashaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AashaApplication.class, args);
+	}
+
+}
